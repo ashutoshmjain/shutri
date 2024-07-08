d18feb9808b34ef56b4cee3647124287b8d3fba3
ashutosh jain <ashutoshmjain@gmail.com>
2024-07-08 11:54:32 -0700

    published nostr feed

---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW([{ url: "https://cdn.npubpro.com/index.js", revision: "1720463822942" }, { url: "/sw.js", revision: "1720463822943" }, { url: "/manifest.webmanifest", revision: "1720463822944" }]);
+  
