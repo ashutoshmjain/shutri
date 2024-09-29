5db83f835767299fa6484c3aed3a7f2849cee409
ashutosh jain <ashutoshmjain@gmail.com>
2024-09-29 13:00:54 -0700

    new theme mnml

--- nostr/sw.js
+++
@@ -1,4 +0,0 @@
-
-    importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1723767617393" }, { url: "/sw.js", revision: "1723767617394" }, { url: "/manifest.webmanifest", revision: "1723767617395" }] });
-  
---
+++ sw.js
@@ -0,0 +1,4 @@
+
+    importScripts("https://cdn.npubpro.com/index.js");
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1727638791173" }, { url: "/sw.js", revision: "1727638791174" }, { url: "/manifest.webmanifest", revision: "1727638791175" }] });
+  
