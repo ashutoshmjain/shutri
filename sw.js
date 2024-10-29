c44e436466bd5b41dc383cd2374dc01d33ec22c7
ashutosh jain <ashutoshmjain@gmail.com>
2024-10-28 18:13:36 -0700

    testing

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
 
     importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1727638791173" }, { url: "/sw.js", revision: "1727638791174" }, { url: "/manifest.webmanifest", revision: "1727638791175" }] });
+    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1730161700311" }, { url: "/sw.js", revision: "1730161700312" }, { url: "/manifest.webmanifest", revision: "1730161700313" }] });
   
