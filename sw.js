b536c656b76a129ea69ac645a78d3eabbf2fb114
ashutosh jain <ashutoshmjain@gmail.com>
2025-06-20 14:50:34 -0700

    gita audio

--- sw.js
+++
@@ -1,4 +0,0 @@
-
-    importScripts("https://cdn.npubpro.com/index.js");
-    self.nostrSite.startSW({ index: "https://cdn.npubpro.com/index.js", precacheEntries: [{ url: "https://cdn.npubpro.com/index.js", revision: "1730161700311" }, { url: "/sw.js", revision: "1730161700312" }, { url: "/manifest.webmanifest", revision: "1730161700313" }] });
-  
